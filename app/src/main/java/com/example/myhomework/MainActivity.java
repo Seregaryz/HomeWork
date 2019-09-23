6dc1ac6650b12c0256d75e801987e0b895b75512
Sergey <seregaryz@gmail.com>
2019-09-23 20:11:55 +0300

    Add task 1

--- app/src/main/java/com/example/myhomework/MainActivity.java
+++ app/src/main/java/com/example/myhomework/MainActivity.java
@@ -2,14 +2,29 @@ package com.example.myhomework;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.example.myhomework.R;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button next1;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        next1 = (Button)findViewById(R.id.btn_next1);
+        next1.setOnClickListener(v -> {
+            Intent intent = new Intent(this, com.example.myhomework.SecondActivity.class);
+            startActivity(intent);
+        });
     }
 
+    public void onButtonClick(View v){
+
+    }
 }
